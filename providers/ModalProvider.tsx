36b493d9db8f792b6403b4e9910082076f5c1066
JamesWoo <JamesWoo>
2023-06-09 23:06:16 +0900

    add Uploadmodal

--- providers/ModalProvider.tsx
+++ providers/ModalProvider.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import AuthModal from '@/components/AuthModal'
+import UploadModal from '@/components/UploadModal'
 import { useEffect, useState } from 'react'
 
 export default function ModalProvider() {
@@ -17,6 +18,7 @@ export default function ModalProvider() {
   return (
     <>
       <AuthModal />
+      <UploadModal />
     </>
   )
 }
